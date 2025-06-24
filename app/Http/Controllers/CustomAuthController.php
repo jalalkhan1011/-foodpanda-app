df654feeb4f7ece23d778411206f17f3e357ef00
jalalkhan1011 <jalal.khan981@gmail.com>
2025-06-25 01:40:05 +0600

    1st commit with some custom functiona and route

---
+++ app/Http/Controllers/CustomAuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CustomAuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+
+        $credentials = $request->only('email', 'password');
+
+        if (auth()->attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+
+    public function ssoLogin(Request $request)
+    {
+        if (auth()->check()) {
+             return redirect()->intended('dashboard');
+        } else {
+            return redirect(route('login'));
+        }
+    }
+
+    public function ssoLogout(Request $request)
+    {
+        auth()->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+}
